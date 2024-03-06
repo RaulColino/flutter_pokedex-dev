2a8588021734d5955fc42381cc2ecb311dc4826b
rcs <r.colinos.2016@alumnos.urjc.es>
2024-03-06 10:53:00 +0100

    Base project setup. Generated custom theme though theme_kit Mason Brick

---
+++ packages/devest_ui/lib/src/typography/dv_font_family.dart
@@ -0,0 +1,7 @@
+class DvFontFamily {
+  const DvFontFamily._(this.name);
+  final String name;
+
+  static const DvFontFamily poppins = DvFontFamily._("Poppins");
+  //static const DvFontFamily inter = DvFontFamily._("Inter");
+}
