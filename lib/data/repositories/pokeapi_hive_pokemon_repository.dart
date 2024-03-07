2aded719c36babb80ad50db1bcf8c941ec128ec0
rcs <r.colinos.2016@alumnos.urjc.es>
2024-03-07 18:52:34 +0100

    made some cleanup

--- lib/data/repositories/pokeapi_hive_pokemon_repository.dart
+++ lib/data/repositories/pokeapi_hive_pokemon_repository.dart
@@ -1,26 +1,23 @@
-//join data and return all pokemon
-
 import 'package:flutter_pokedex/data/datasources/local/hive_pokemon_captured_datasource.dart';
 import 'package:flutter_pokedex/data/datasources/remote/pokeapi_pokemon_datasource.dart';
 import 'package:flutter_pokedex/domain/entities/app_exception.dart';
 import 'package:flutter_pokedex/domain/entities/pokemon_entity.dart';
 import 'package:flutter_pokedex/domain/repository_interfaces/i_pokemon_repository.dart';
 import 'package:oxidized/oxidized.dart';
+import 'package:http/http.dart' as http;
 
 class PokemonRepository implements IPokemonRepository {
-  final PokeapiDatasource pokemonDatasource = PokeapiDatasource();
+  final PokeapiDatasource pokemonDatasource = PokeapiDatasource(httpClient: http.Client());
   final HivePokemonCapturedDatasource capturedDatasource = HivePokemonCapturedDatasource();
 
   //getPokemonList
   @override
   Future<Result<List<PokemonEntity>, AppException>> getPokemonList() async {
-    final Result<List<(String, String)>, AppException> result =
-        await pokemonDatasource.getPokemonList();
+    final result = await pokemonDatasource.getPokemonList();
     final List<(String, String)> list = result.unwrap();
 
     final List<Future<PokemonEntity>> futureList = list.map((elem) async {
-      final Result<PokemonEntity, AppException> pokemon =
-          await pokemonDatasource.getPokemonDetailsByName(elem.$1);
+      final pokemon = await pokemonDatasource.getPokemonDetailsByName(elem.$1);
       return pokemon.unwrap();
     }).toList();
 
