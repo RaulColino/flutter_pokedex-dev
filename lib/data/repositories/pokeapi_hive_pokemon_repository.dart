bcbb8ea3be1fe56e1875ed177bdbcf8172958a77
rcs <r.colinos.2016@alumnos.urjc.es>
2024-03-07 17:00:06 +0100

    Made some cleanup in data layer

--- lib/data/repositories/pokeapi_hive_pokemon_repository.dart
+++ lib/data/repositories/pokeapi_hive_pokemon_repository.dart
@@ -33,21 +33,4 @@ class PokemonRepository implements IPokemonRepository {
   Future<Result<List<PokemonEntity>, AppException>> getPokemonCaptured() {
     return capturedDatasource.getCapturedPokemon();
   }
-
-  // @override
-  // Future<String> getPokemonDetailsByName(String name) {}
-
-  // Future<String> getPokemonDetailsByName(string name) async {
-  //   return await pokeapiDatasource.getPokemonDetailsByName(name);
-  // }
-
-  // //Get all pokemon
-  // Future<String> getPokemonList() async {
-  //   return await pokeapiDatasource.getPokemonList();
-  // }
-
-  // //Get captured pokemon
-  // Future<String> getCaptured(String name) async {
-  //   return await localDbDatasource.getCapturedPokemon();
-  // }
 }
